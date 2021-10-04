7fe907b58b062632a88995c7dbd578596f38f8b2
eks93 <eks93@drexel.edu>
2021-10-04 11:01:40 -0400

    udpated codekit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,6 +15,5 @@ console.log(heroHeight.clientWidth + "px is the width of the hero section");
 // name of the timeline | do you want to start at the end or the beginning of the timeline?  | What do you want to animate? | { how long is the animation? | what do you want to do?}
 // mainTL.to("#hero",{duration:2, alpha:0});
 
-mainTL.from("#hero >div article",{duration:1, alpha:0, y:-heroHeight.clientHeight})
-       .from("#hero h1",{duration:0.5, alpha:0, y:"+=200", rotation: 180}, "madeUp")
-       .from("#hero h2",{duration:1, alpha:0, y:"+=200"}, "madeUp");
+mainTL.from("#hero >div article",{duration:1, alpha:0})
+       .from("#hero h1",{duration:0.5, alpha:0, y:"+=200"}, "madeUp")
