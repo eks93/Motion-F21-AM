5125e539c92c8243afd645026e07845e500290a8
eks93 <eks93@drexel.edu>
2021-10-06 06:29:47 -0400

    Animated Text
    added animated text

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,19 +1,25 @@
 import { gsap } from "gsap";
 
 
+
 // const duration = 0.75;
 
 const mainTL = gsap.timeline();
 
 // grab the element you want to get the height or width from
 
-const heroHeight = document.querySelector("#hero");
 
-console.log(heroHeight.clientHeight + "px is the height of the hero section");
-console.log(heroHeight.clientWidth + "px is the width of the hero section");
+
+//console.log(heroHeight.clientHeight + "px is the height of the hero section");
+//console.log(heroHeight.clientWidth + "px is the width of the hero section");
 
 // name of the timeline | do you want to start at the end or the beginning of the timeline?  | What do you want to animate? | { how long is the animation? | what do you want to do?}
 // mainTL.to("#hero",{duration:2, alpha:0});
 
-mainTL.from("#hero >div article",{duration:1, alpha:0})
-       .from("#hero h1",{duration:0.5, alpha:0, y:"+=200"}, "madeUp")
+//mainTL.from("#hero >div article",{duration:1, alpha:0})
+// .from("#title", {duration:2, blur:8, ease: "none"});
+
+
+mainTL.from("#title", {duration: 5, alpha:0})
+.from("#title", {duration:2, blur:8, ease: "none"});
+       
