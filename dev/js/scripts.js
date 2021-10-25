d3bc4d6b5ab2f7d678485058c0cca20951f8de78
eks93 <eks93@drexel.edu>
2021-10-25 04:33:45 -0400

    Animating Preloader
    first 2 lines

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,74 @@
 import { gsap } from "gsap";
 
-
-
-// const duration = 0.75;
-
+//this is a global variable ; aka global constant
+//whatever js file you make, they'll be aware of this constant 
 const mainTL = gsap.timeline();
 
-// grab the element you want to get the height or width from
+
+//mainTL.from("#right-square",{duration:1, alpha:0});
+
+
+//always include return tl; , and the constant line of code, you can make up names for functions
+//constants within fuctions are local and only available/accesible within their specific functions
+
+function fadeInSquare(){
+    const tl = gsap.timeline();
+    tl.from("#right-square",{duration:1, alpha:0});
+    return tl;
+}
+
+
+
+
+//add functions to main tl chronologically and after the last one include ";"
+//this is the parent of the functions and dictates which happens first
+//if you want 2 things to happen at the same time:
+// after purple brackets, put: "same" within the yellow one on BOTH elements
+mainTL.add(fadeInSquare());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use this too
+// const mainTL = gsap.timeline();
+
+
 
 
 
@@ -19,11 +81,11 @@ const mainTL = gsap.timeline();
 //mainTL.from("#hero >div article",{duration:1, alpha:0})
 // .from("#title", {duration:2, blur:8, ease: "none"});
 
-
-mainTL.from("#hero article", {duration: 2, alpha:0, y:"+=1000"})
-    .from("#title", {duration:0.25, alpha:0, blur:10, scale:1.5})
-    .to("#title", {duration:1, scale:1})
-    .from("splittext", {duration:1, y:"+=20", stagger:0.5});
+//use this one
+// mainTL.from("#hero article", {duration: 2, alpha:0, y:"+=1000"})
+  //  .from("#title", {duration:0.25, alpha:0, blur:10, scale:1.5})
+  //  .to("#title", {duration:1, scale:1})
+  //  .from("splittext", {duration:1, y:"+=20", stagger:0.5});
 
 
 
