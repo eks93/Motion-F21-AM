f72c4fa111728474b14efb64739af181bb17cc3d
eks93 <eks93@drexel.edu>
2021-12-08 03:39:57 -0500

    Appel Farm Logo
    changes made to this file (made copy of original version); logo progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,144 +1,167 @@
 import { gsap } from "gsap";
-// import { GSDevTools } from "gsap/GSDevTools";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-gsap.registerPlugin(MorphSVGPlugin);
+import { GSDevTools } from "gsap/GSDevTools";
+// import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { SplitText } from "gsap/SplitText";
+gsap.registerPlugin(GSDevTools, DrawSVGPlugin, SplitText);
 
-// gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
+
 
 const mainTL = gsap.timeline();
 
 
-function moveSquares(){
+
+
+function drawShape(){
   const tl = gsap.timeline();
-  tl.from("#left-square",{duration: 1, x:"-=600"},"makeShape")
-  .from("#right-square",{duration: 1, x:"+=1200"},"makeShape")
-  .from("#top-square",{duration: 1, y:"-=600"},"makeShape")
-  .from("#bottom-square",{duration: 1, y:"+=600"},"makeShape");
+  tl.from("#logo", {duration: 3, drawSVG: "0%"});
   return tl;
 }
 
-function expand(){
+function rainbowDraw(){
   const tl = gsap.timeline();
-  tl.from("#start-one", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-one", {duration: 0.5, ease:"slow", morphSVG:"#end-one"}, "move")
+  tl.from("#orange", {duration: 1, drawSVG: "0%"})
+  .from("#pink", {duration: 1, drawSVG: "0%", stagger:0.5})
+  .from("#purple", {duration: 1, drawSVG: "0%", stagger:0.5})
+  .from("#blue", {duration: 1, drawSVG: "0%", stagger:0.5})
   return tl;
 }
 
-function expandTwo (){
+function nameDrop(){
   const tl = gsap.timeline();
-  tl.from("#start-two", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-two", {duration: 0.5, ease:"slow", morphSVG:"#end-two"});
+  tl.from("#f", {duration: 0.5, y:"-=1000", ease: "bounce.out"})
+  .from("#a-two", {duration: 0.3, y:"-=1000", ease: "back.out(0.4)"})
+  .from("#r", {duration: 0.3, y:"-=1000", ease: "back.out(0.4)"})
+  .from("#m", {duration: 0.3, y:"-=1000", ease: "back.out(0.4)"})
+  .from("#a", {duration: 0.3, y:"-=1000", ease: "back.out(0.5)"})
+  .from("#p", {duration: 0.1, y:"-=1000", ease: "back.out(0.5)"})
+  .from("#p-two", {duration: 0.2, y:"-=1000", ease: "back.out(0.5)"})
+  .from("#e", {duration: 0.3, y:"-=1000", ease: "back.out(0.5)"})
+  .from("#l", {duration: 0.3, y:"-=1000", ease: "back.out(0.5)"});
   return tl;
 }
 
-function expandTwoAgain (){
+function fadeInLine(){
   const tl = gsap.timeline();
-  tl.from("#start-start-two", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-start-two", {duration: 0.5, ease:"slow", morphSVG:"#end-end-two"});
-  return tl;
-}
-
-function expandThree (){
-  const tl = gsap.timeline();
-  tl.from("#start-three", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-three", {duration: 0.5, morphSVG:"#end-three"}, "move-three")
-  .from("#start-start-three", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-start-three", {duration: 0.5, morphSVG:"#end-end-three"}, "move-three");
-  return tl;
-}
-
-function expandFour (){
-  const tl = gsap.timeline();
-  tl.from("#start-four", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
-  .to("#start-four", {duration: 0.75, morphSVG:"#end-four"});
-  return tl;
-}
-
-function expandFive (){
-  const tl = gsap.timeline();
-  tl.from("#start-five", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
-  .to("#start-five", {duration:0.25, morphSVG:"#end-five"}, "move-five")
-  .from("#start-start-five", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
-  .to("#start-start-five", {duration: 0.25, morphSVG:"#end-end-five"}, "move-five")
-  return tl;
-}
-
-function expandSix (){
-  const tl = gsap.timeline();
-  tl.from("#start-six", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
-  .to("#start-six", {duration:0.25, morphSVG:"#end-six"}, "move-six")
-  .from("#start-start-six", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
-  .to("#start-start-six", {duration: 0.25, morphSVG:"#end-end-six"}, "move-six");
-  return tl;
-}
-
-function flyIn(){
-  const tl = gsap.timeline();
-  tl.from("#fly-in-sq", {duration: 0.75, x:"+=1000"}, "fly");
-  return tl;
-}
-
-function flyInTwo(){
-  const tl = gsap.timeline();
-  tl.from("#fly-in-sq-two", {duration: 0.75, x:"+=1000"}, "fly");
-  return tl;
-}
-
-function rotateShape(){
-   const tl = gsap.timeline();
-    tl.to("#logo", {duration: 0.5, rotation: "90_ccw", transformOrigin: "50% 50%"});
-   return tl;
- }
-
-function moveDown(){
-  const tl = gsap.timeline();
-  tl.to("#logo", {duration: 0.5, ease:"power1.inOut", y:"+=250", x:"-=500", scale:0.25 })
+  tl.from("#slogan", {duration: 2 })
   return tl;
 }
 
 
-mainTL.add(moveSquares())
-.add(expand())
-.add(expandTwo(), "same")
-.add(expandTwoAgain(), "same")
-.add(expandThree(), "sameThree")
-.add(expandFour(), "sameThree")
-.add(expandFive())
-.add(expandSix(), "sameTwo")
-.add(flyIn(), "sameTwo")
-.add(flyInTwo())
-.add(rotateShape())
-.add(moveDown());
 
 
-// GSDevTools.create();
+
+mainTL.add(drawShape())
+.add(rainbowDraw())
+.add(nameDrop())
+.add(fadeInLine());
+
+
+
+GSDevTools.create();
 
 
 
 
+// function moveSquares(){
+//   const tl = gsap.timeline();
+//   tl.from("#left-square",{duration: 1, x:"-=600"},"makeShape")
+//   .from("#right-square",{duration: 1, x:"+=1200"},"makeShape")
+//   .from("#top-square",{duration: 1, y:"-=600"},"makeShape")
+//   .from("#bottom-square",{duration: 1, y:"+=600"},"makeShape");
+//   return tl;
+// }
+
+// function expand(){
+//   const tl = gsap.timeline();
+//   tl.from("#start-one", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-one", {duration: 0.5, ease:"slow", morphSVG:"#end-one"}, "move")
+//   return tl;
+// }
+
+// function expandTwo (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-two", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-two", {duration: 0.5, ease:"slow", morphSVG:"#end-two"});
+//   return tl;
+// }
+
+// function expandTwoAgain (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-start-two", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-start-two", {duration: 0.5, ease:"slow", morphSVG:"#end-end-two"});
+//   return tl;
+// }
+
+// function expandThree (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-three", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-three", {duration: 0.5, morphSVG:"#end-three"}, "move-three")
+//   .from("#start-start-three", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-start-three", {duration: 0.5, morphSVG:"#end-end-three"}, "move-three");
+//   return tl;
+// }
+
+// function expandFour (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-four", {autoAlpha: 0, duration: 0.2}, {alpha: 1})
+//   .to("#start-four", {duration: 0.75, morphSVG:"#end-four"});
+//   return tl;
+// }
+
+// function expandFive (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-five", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
+//   .to("#start-five", {duration:0.25, morphSVG:"#end-five"}, "move-five")
+//   .from("#start-start-five", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
+//   .to("#start-start-five", {duration: 0.25, morphSVG:"#end-end-five"}, "move-five")
+//   return tl;
+// }
+
+// function expandSix (){
+//   const tl = gsap.timeline();
+//   tl.from("#start-six", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
+//   .to("#start-six", {duration:0.25, morphSVG:"#end-six"}, "move-six")
+//   .from("#start-start-six", {autoAlpha: 0, duration: 0.25}, {alpha: 1})
+//   .to("#start-start-six", {duration: 0.25, morphSVG:"#end-end-six"}, "move-six");
+//   return tl;
+// }
+
+// function flyIn(){
+//   const tl = gsap.timeline();
+//   tl.from("#fly-in-sq", {duration: 0.75, x:"+=1000"}, "fly");
+//   return tl;
+// }
+
+// function flyInTwo(){
+//   const tl = gsap.timeline();
+//   tl.from("#fly-in-sq-two", {duration: 0.75, x:"+=1000"}, "fly");
+//   return tl;
+// }
+
+// function rotateShape(){
+//    const tl = gsap.timeline();
+//     tl.to("#logo", {duration: 0.5, rotation: "90_ccw", transformOrigin: "50% 50%"});
+//    return tl;
+//  }
+
+// function moveDown(){
+//   const tl = gsap.timeline();
+//   tl.to("#logo", {duration: 0.5, ease:"power1.inOut", y:"+=250", x:"-=500", scale:0.25 })
+//   return tl;
+// }
 
 
-
-// landing page code
-// const mainTL = gsap.timeline();
-
-
-//console.log(heroHeight.clientHeight + "px is the height of the hero section");
-//console.log(heroHeight.clientWidth + "px is the width of the hero section");
-
-// name of the timeline | do you want to start at the end or the beginning of the timeline?  | What do you want to animate? | { how long is the animation? | what do you want to do?}
-// mainTL.to("#hero",{duration:2, alpha:0});
-
-//mainTL.from("#hero >div article",{duration:1, alpha:0})
-// .from("#title", {duration:2, blur:8, ease: "none"});
-
-//use this one
-// mainTL.from("#hero article", {duration: 2, alpha:0, y:"+=1000"})
-  //  .from("#title", {duration:0.25, alpha:0, blur:10, scale:1.5})
-  //  .to("#title", {duration:1, scale:1})
-  //  .from("splittext", {duration:1, y:"+=20", stagger:0.5});
-
-
-
-       
+// mainTL.add(moveSquares())
+// .add(expand())
+// .add(expandTwo(), "same")
+// .add(expandTwoAgain(), "same")
+// .add(expandThree(), "sameThree")
+// .add(expandFour(), "sameThree")
+// .add(expandFive())
+// .add(expandSix(), "sameTwo")
+// .add(flyIn(), "sameTwo")
+// .add(flyInTwo())
+// .add(rotateShape())
+// .add(moveDown());      
        
